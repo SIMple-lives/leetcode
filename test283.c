b67367c8f19232025724a1eb1476e60ef3fb5751
SIMple-lives <1305969480@qq.com>
2023-11-17 22:09:40 +0800

    leetcode283

---
+++ test283.c
@@ -0,0 +1,176 @@
+#include<stdio.h>
+
+/* void moveZeroes(int* nums, int numsSize) 
+{
+    int k=0;
+    int j=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
+        {
+            k=i;
+        }
+        else if(nums[i]==0)
+        {
+            j=i;
+        }
+        if(k<j)
+        {
+            nums[j]=nums[j]^nums[k];
+            nums[k]=nums[j]^nums[k];
+            nums[j]=nums[j]^nums[k];
+        }
+    }
+} */
+
+/* void moveZeroes(int* nums, int numsSize) 
+{
+    int k=numsSize;//0的位置
+    int j=numsSize;//非0的位置.
+    int tmp=0;
+    for(int i=numsSize-1;i>0;i--)
+    {
+        if(nums[i]==0)
+        {
+            k=i;
+        }
+        else if(nums[i]!=0)
+        {
+            j=i;
+        }
+        if(j>k)
+        {
+           int temp=nums[k];
+           for(int l=j;l>k;l--)
+           {
+            nums[l]=nums[l-1];
+
+           }
+        }
+        k=numsSize;
+        j=numsSize;
+    }
+} */
+
+/* void moveZeroes(int* nums, int numsSize) //0的位置搞反了.
+{
+    int k=0;//0的位置
+    int j=0;//非零的位置
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==0)
+        {
+            k=i;
+        }
+        else if(nums[i]!=0)
+        {
+            j=i;
+        }
+        if(j<k)
+        {
+            int tmp=nums[k];
+            for(int l=k;l>=j;l--)
+            {
+                nums[l]=nums[l-1];
+            }
+            nums[j]=tmp;
+        }
+        k=0;
+        j=0;
+    }
+} */
+
+/* void moveZeroes(int* nums, int numsSize) //0的位置搞反了.
+{
+    int k=numsSize;//从后往前记录为0的值
+    int j=numsSize;//从后往前记录！0值.
+    for(int i=numsSize-1;i>0;i++)
+    {
+        if(nums[i]==0)
+        {
+            k=i;
+        }
+        else if(nums[i]!=0)
+        {
+            j=i;
+        }
+        if(j>)
+    }
+}
+ */
+/* void moveZeroes(int* nums, int numsSize) 
+{
+    int k=numsSize;
+    for(int i=numsSize-1;i>=0;i--)
+    {
+        if(nums[i]==0)
+        {
+
+        }
+    }
+} */
+
+void moveZeroes(int* nums, int numsSize) 
+{
+    int k=0;//0的位置
+    int j=0;//非零的位置
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==0)
+        {
+            k=i;
+        }
+        else if(nums[i]!=0)
+        {
+            j=i;
+        }
+        if(j>k)
+        {
+            int tmp=nums[j];
+            for(int l=j;l>=k;l--)
+            {
+                nums[l]=nums[l-1];
+            }
+            nums[k]=tmp;
+        }
+        k=0;
+        j=0;
+    }
+    int m=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==0)
+        {
+            m=i-1;
+            break;
+        }
+    }
+    int left=0;
+    int right=m;
+    while(left<right)
+    {
+        nums[left]=nums[left]^nums[right];
+        nums[right]=nums[left]^nums[right];
+        nums[left]=nums[left]^nums[right];
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    int n=0;
+    scanf("%d",&n);
+    int nums[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&nums[i]);
+    }
+    moveZeroes(nums,n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+    return 0;
+}
