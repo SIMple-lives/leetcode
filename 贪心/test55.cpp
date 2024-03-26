1a505b5c836415d8e21f0de1a9f99146495e1bbe
SIMple-lives <1305969480@qq.com>
2024-03-26 18:55:28 +0800

    leet55

---
+++ 贪心/test55.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    bool canJump(std::vector<int>& nums) {
+        int area=0;
+        if(nums.size()==1)
+        return true;
+        for(int i = 0;i<=area;i++)
+        {
+            area=std::max(area,i+nums[i]);
+            if(area>=nums.size()-1)
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    Solution s;
+    std::vector<int> nums={2,3,1,1,4};
+    std::cout<<s.canJump(nums)<<std::endl;
+    return 0;
+}
