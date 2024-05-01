17b86bd21577a41de30788b3db2c39a31da20d4e
SIMple-lives <1305969480@qq.com>
2024-05-01 22:07:40 +0800

    leet155

---
+++ 栈与队列/test155.cpp
@@ -0,0 +1,30 @@
+#include <stack>
+#include <algorithm>
+#include <limits.h>
+
+class MinStack {
+    std::stack<int> x_stack;
+    std::stack<int> min_stack;
+public:
+    MinStack() {
+        min_stack.push(INT_MAX);
+    }
+    
+    void push(int x) {
+        x_stack.push(x);
+        min_stack.push(std::min(min_stack.top(), x));
+    }
+    
+    void pop() {
+        x_stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return x_stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
