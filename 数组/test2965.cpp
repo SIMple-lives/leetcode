05ef3b5a2d72123cc6b803405df022e086f8553b
SIMple-lives <1305969480@qq.com>
2024-06-01 21:04:20 +0800

    leet2965

---
+++ 数组/test2965.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+
+class Solution
+{
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid)
+    {
+        map<int, int> num;
+        int more = 0;
+        int lost = 0;
+        int n = grid.size();
+        // 遍历 找到多出来的数字
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                num[grid[i][j]]++;
+                if (num[grid[i][j]] == 2) {
+                    more = grid[i][j];
+                }
+            }
+        };
+        // 遍历找出缺少的数字
+        for (auto it = num.begin(); it != num.end(); it++) {
+            if (it->first + 1 != (++it)->first) {
+                // cout << (it--)->first << endl;
+                lost = (--it)->first + 1;
+                break;
+            }
+            it--;
+        }
+        if (lost > n * n) {
+            lost = 1;
+        }
+        vector<int> ans;
+        ans.push_back(more);
+        ans.push_back(lost);
+        return ans;
+    }
+};
+
