a91e51d616c0626b2818bc92810ddac94de60434
SIMple-lives <1305969480@qq.com>
2024-06-29 17:04:01 +0800

    leet2807

---
+++ 链表/test2807.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) 
+    {
+        ListNode *node = head;
+        while(node->next)
+        {
+            node->next = new ListNode(std::__gcd(node->val,node->next->val),node->next);
+            node=node->next->next;
+        }
+        return head;
+    }
+};
