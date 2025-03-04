70d7bb5474cd57e87701edea7e505effe1258a2c
SIMple-lives <1305969480@qq.com>
2025-03-04 21:31:35 +0800

    add equal_range

--- 二分/34.cpp
+++ 二分/34.cpp
@@ -22,7 +22,30 @@ class Solution {
         if(start == nums.size() || nums[start] != target) {
             return {-1 , -1};
         }
-        int end = lower_bound(nums, target + 1) - 1;
-        return {start, end};
+        // int end = lower_bound(nums, target + 1) - 1;
+        auto end =upper_bound(nums.begin(), nums.end(), target) -1;
+        int end_ = end - nums.begin() - 1;
+        return {start, end_};
     }
 };
+
+class solution {
+    public:
+        vector<int> searchRange(vector<int>& nums, int target) {
+            auto ret = std::equal_range(nums.begin(), nums.end(), target);
+    
+            // 检查目标值是否存在
+            if (ret.first == ret.second) {
+                // 目标值不存在，返回 {-1, -1}
+                return {-1, -1};
+            }
+    
+            // 计算目标值第一次出现的索引
+            int firstIndex = ret.first - nums.begin();
+            // 计算目标值最后一次出现的索引
+            int lastIndex = ret.second - nums.begin() - 1;
+    
+            // 返回包含第一次和最后一次出现位置的向量
+            return {firstIndex, lastIndex};
+        }
+    };
